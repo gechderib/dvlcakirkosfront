ba2b46adc620f6a468382b84b248dc8b36bcc6ac
yordanosmoges21 <yordanosmoges21@gmail.com>
2024-09-04 22:13:07 -0700

    chnaging to kirkos url

--- src/components/FloatingButton.jsx
+++ src/components/FloatingButton.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 const FloatingButton = ({isOther=false}) => {
  const navigate = useNavigate()
   return (
-    <div className={`fixed  ${isOther?"right-32 top-20":"top-32 left-20"}`}>
+    <div className={`fixed  ${isOther?"right-32 bottom-20":"top-32 left-20"}`}>
       <button onClick={()=>{navigate("/")}} className="bg-gradient-to-r from-indigo-500 to-purple-600 hover:bg-blue-700 text-white font-bold py-3 px-8 rounded-full shadow-xl">
         Home {" >>"}
       </button>
